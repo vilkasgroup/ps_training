cf157645e91f3e2be91ede338641ef9117b17f4a
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2020-01-15 11:19:17 +0200

    Adding finish lang translations

---
+++ translations/fi.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{training}prestashop>training_04cbcfca7894eb775449aaee8c9d1b5d'] = 'Training in finnish';
+$_MODULE['<{training}prestashop>training_a629289cce1bad6b529711f845421dcc'] = 'Training module in finnish';
